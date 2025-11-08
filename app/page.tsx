09c4543107803ddac601fb179ff862d14f2980c2
Marián Hollý <marian.holly7117@gmail.com>
2025-11-08 23:37:04 +0100

    framer motion installed

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,16 @@
 import { useTheme } from "next-themes";
 import { Moon, Sun } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+
 import { ChartAreaInteractive } from "@/components/interactive-area-chart";
+import { ChartPieInteractive } from "@/components/interactive-pie-chart";
 
 export default function Home() {
   const { setTheme } = useTheme();
 
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-screen bg-background mb-24">
       <nav className="flex flex-row justify-center gap-4 my-8">
         <Button variant="outline" onClick={() => setTheme("light")}>
           <Sun className="h-4 w-4" />
@@ -23,6 +26,11 @@ export default function Home() {
       <main className="container mx-auto">
         <ChartAreaInteractive />
       </main>
+      <Separator className="my-8" />
+
+      <div className="container mx-auto">
+        <ChartPieInteractive />
+      </div>
     </div>
   );
 }
